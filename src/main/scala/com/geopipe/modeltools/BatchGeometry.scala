b9fd547c7b376c5e8e569507251d011b4168619e
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2016-10-28 15:13:41 -0400

    More robust argument parsing

--- src/main/scala/com/geopipe/modeltools/BatchGeometry.scala
+++ src/main/scala/com/geopipe/modeltools/BatchGeometry.scala
@@ -2,7 +2,13 @@ package com.geopipe.modeltools
 
 import scala.xml._
 import scala.xml.transform._
-import org.apache.spark._
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+class BatchedGeometryRewriter(collada:Node) extends RewriteRule {
+	
+}
 
 object BatchGeometry {
 	def makeImagesUnique(collada:Node):Node = {
@@ -15,16 +21,23 @@ object BatchGeometry {
 		transformer(collada)
 	}
 	
-	def main(args:Array[String]):Unit = {
-		val sc = SparkContext.getOrCreate
-		val dirPattern = args(0)
-		val filesToProcess = sc.wholeTextFiles(dirPattern)
+	def processInputFiles(filesToProcess:RDD[(String,String)], outputSuffix:String, jsonSuffix:String) = {
 		filesToProcess.foreach{
 			case (path, content) => 
 				val collada = XML.loadString(content)
 				val withUniqueImages = makeImagesUnique(collada)
 				val withUniqueEffects = makeEffectsUnique(withUniqueImages)
 				Console.println(withUniqueEffects)
+		}
+	}
+	
+	def main(args:Array[String]):Unit = {
+		args match {
+			case Array(dirPattern, outputSuffix, jsonSuffix) =>
+				processInputFiles(SparkContext.getOrCreate.wholeTextFiles(dirPattern), outputSuffix, jsonSuffix)
+			case _ => 
+				Console.err.println("BatchGeometry takes 3 arguments: dirPattern, outputSuffix, jsonSuffix")
+				System.exit(-1)
 		}
 	}
 }
